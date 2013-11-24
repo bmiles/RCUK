3e410e99887258537015a062fa74b74c8ee5025c
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-24 13:11:31 +0000

    Search returns a list of user objects

--- api/people_search.py
+++ api/people_search.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-from gtr import request
+from gtr import request, db
 
 
 # 1: NumOrgs
@@ -24,6 +24,10 @@ person_score = read_csv_file('data/person_score.csv')
 
 
 def search(topic):
+    def get_person(pid):
+        person = db.persons.find_one({"id": pid})
+        person['score'] = person_score.get(pid, 0.0)
+        return person
     projects_json = request(api_stem + "projects",
                             {'q': 'pro.a=%s' % topic, 's': 100})
 
@@ -34,7 +38,7 @@ def search(topic):
                 persons.append(link.get('href').split('/')[-1])
 
     persons = sorted(set(persons), key=lambda person: -person_score[person])
-    return [{person: person_score.get(person, 0)} for person in persons]
+    return [get_person(pid) for pid in persons]
 
 
 def read_json_file(filename):
